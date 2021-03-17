c1d209236a263435df3820e4f70bd216ef99e822
Stephen Adesina <bauerstevenson@gmail.com>
2021-03-16 21:52:39 -0400

    testing

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+# INFO
+This is a blog template built on the django framework.
+
+
+# RUN
 run manage.py runserver in cmd to view
--- commit.sh
+++ commit.sh
@@ -1,3 +1,10 @@
+printf "What is your default branch(main or master):"
+read branch
+
+printf "\nWhat commit message would you like to use:"
+read message
+
+
 git add .
-git commit -m READ
-git push -u origin master
+git commit -m "$message"
+git push -u origin $branch
